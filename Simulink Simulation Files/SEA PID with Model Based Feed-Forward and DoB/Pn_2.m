98245ea1cd454eeb1793a7121d7fb0dd0f072c7e
erimcanozcinar <erim.ozcinar@ozu.edu.tr>
2021-01-12 10:54:18 +0300

    Pn.m file is chanced
    Pn.m file inside the Feedforward simulation file is changed to Pn_2.m file.

--- Simulink Simulation Files/SEA PID with Model Based Feed-Forward and DoB/Pn_2.m
+++ Simulink Simulation Files/SEA PID with Model Based Feed-Forward and DoB/Pn_2.m
@@ -1,3 +1,5 @@
+%
+% Run this file first then run the Simulink file (SEA_Feedforward_DOB.slx)
 clc
 close
 
@@ -16,17 +18,16 @@ Pl = tf(1, [Jl (Bl + Benv) Kenv]);
 
 P = (Pm/N)/(1 + Ks*Pm/(N*N) + Ks*Pl);
 
-Wff = 628*2*pi;
-
-Q1 = tf(Wff, [1 Wff]);
-
-FF = inv(P)*Q1^2;
-
 Wc = 1600*2*pi;
+Wv = 600*2*pi;
+
+Q1 = tf(Wv^2, [1 sqrt(2)*Wv Wv^2]);
 
 Q2 = tf(Wc^2, [1 sqrt(2)*Wc Wc^2]);
 
 Dob = inv(P)*Q2;
+
+FF = inv(P)*Q1;
 
 [numFF,denFF] = tfdata(FF,'v');    % Feedforward
 [numDOB,denDOB] = tfdata(Dob,'v'); % DoB of Feedforward
